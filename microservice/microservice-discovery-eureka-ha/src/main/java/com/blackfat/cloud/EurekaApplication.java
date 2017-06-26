2c36a8eef6cc798983149ed7081a128acd847402
wangfeiyang <wangfeiyang@huaxiafinance.com>
2017-06-26 13:31:50 +0800

    eureka高可用

---
+++ microservice/microservice-discovery-eureka-ha/src/main/java/com/blackfat/cloud/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.blackfat.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangfeiyang
+ * @desc 使用Eureka做服务发现
+ * @create 2017/5/19-15:55
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
