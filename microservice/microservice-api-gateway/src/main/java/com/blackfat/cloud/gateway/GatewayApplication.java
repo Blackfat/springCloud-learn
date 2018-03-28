03caef8e7f6cfce9decf40e13b1f8b9a1b9c11ee
wangfeiyang <wangfeiyang@huaxiafinance.com>
2018-03-28 10:35:54 +0800

    网管添加过滤器

--- microservice/microservice-api-gateway/src/main/java/com/blackfat/cloud/gateway/GatewayApplication.java
+++ microservice/microservice-api-gateway/src/main/java/com/blackfat/cloud/gateway/GatewayApplication.java
@@ -1,9 +1,11 @@
 package com.blackfat.cloud.gateway;
 
+import com.blackfat.cloud.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author wangfeiyang
@@ -17,5 +19,11 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
